ef8d131399f8b193f2f7fad23d5067dd23a40221
rborghoff <raymond.borghoff@hva.nl>
2020-03-22 16:34:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/notepad2/ui/EditNotepadActivity.kt
@@ -0,0 +1,23 @@
+package com.example.notepad2.ui
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.notepad2.R
+
+import kotlinx.android.synthetic.main.activity_edit_notepad.*
+
+class EditNotepadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_notepad)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
